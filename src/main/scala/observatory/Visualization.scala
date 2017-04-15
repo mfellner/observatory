c3adb81a7946271a99cec452f20a0a7b6a6087e1
Maximilian Fellner <max.fellner@gmail.com>
2017-04-16 00:00:43 +0200

    feat: implement predictTemperature

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -8,7 +8,9 @@ import com.sksamuel.scrimage.Image
   * 2nd milestone: basic visualization
   */
 object Visualization {
-  val earthRadiusMeters = 6371000F
+  val earthRadiusMeters = 6371000.0
+  val distanceThresholdMeters = 1000.0
+  val p = 2.0
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
@@ -16,17 +18,28 @@ object Visualization {
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-    val p = 2
 
-    temperatures.par.toStream.map {
-      case (Location(lat, lon), temp) => temp
+    val weights = temperatures.par.toStream.map {
+      case (loc, _) =>
+        val d = distance(loc, location)
+        if (d > distanceThresholdMeters)
+          1.0 / pow(d, p)
+        else
+          1.0
     }
 
-    0
-  }
+    val sumOfWeights = weights.sum
 
-  private def weight(interpolated: Location, sampled: Location) = {
+    val sumOfWeightedTemps = temperatures.par.toStream.zip(weights).map {
+      case ((loc, temp), weight) =>
+        val d = distance(loc, location)
+        if (d > distanceThresholdMeters) {
+          weight * temp
+        } else
+          temp
+    }.sum
 
+    sumOfWeightedTemps / sumOfWeights
   }
 
   // https://en.wikipedia.org/wiki/Great-circle_distance
@@ -36,12 +49,12 @@ object Visualization {
     val aLatR = toRadians(a.lat)
     val bLatR = toRadians(b.lat)
 
-    val dividend = sqrt(pow(cos(bLatR) * sin(dLon), 2) +
-      pow(cos(aLatR) * sin(bLatR) - sin(aLatR) * cos(bLatR) * cos(dLon), 2))
+    val dividend = sqrt(pow(cos(bLatR) * sin(dLon), 2.0) +
+      pow(cos(aLatR) * sin(bLatR) - sin(aLatR) * cos(bLatR) * cos(dLon), 2.0))
     val divisor = sin(aLatR) * sin(bLatR) + cos(aLatR) * cos(bLatR) * cos(dLon)
     //    val radians = atan(dividend / divisor)
     val radians = atan2(dividend, divisor)
-    6371000 * radians
+    6371000.0 * radians
   }
 
   /**
--- src/test/scala/observatory/ManipulationTest.scala
+++ src/test/scala/observatory/ManipulationTest.scala
@@ -1,28 +1,50 @@
 package observatory
 
 import org.junit.runner.RunWith
-import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.prop.Checkers
-import org.scalactic.TolerantNumerics
+import org.scalatest.{FunSuite, Matchers}
 
 @RunWith(classOf[JUnitRunner])
-class ManipulationTest extends FunSuite with Checkers {
-  test("distance") {
+class ManipulationTest extends FunSuite with Checkers with Matchers {
+  val london = Location(51.509865, -0.118092)
+  val berlin = Location(52.520008, 13.404954)
+  val munich = Location(48.137154, 11.576124)
+  val dresden = Location(51.0508900, 13.7383200)
+  val potsdam = Location(52.3988600, 13.0656600)
+  val berlin2 = Location(52.537831, 13.375607)
+  val berlin3 = Location(52.537539, 13.372547)
+  val berlin4 = Location(52.535344, 13.374048)
 
-    val london = Location(51.509865, -0.118092)
-    val berlin = Location(52.520008, 13.404954)
-    val d = Visualization.distance(london, berlin)
-    implicit val doubleEquality = TolerantNumerics.tolerantDoubleEquality(2F)
-    assert(d === 930851F)
+  test("distance") {
+    assert(Visualization.distance(berlin, london) - 930851 < 1)
+    assert(Visualization.distance(berlin, munich) - 504300 < 1)
+    assert(Visualization.distance(berlin, dresden) - 504300 < 1)
+    assert(Visualization.distance(berlin, potsdam) - 26644 < 1)
+    assert(Visualization.distance(berlin2, berlin3) - 224 < 1)
   }
 
-  test("predictTemperature") {
-    val path = "/2000-local.csv"
-    val file = Main.getClass.getResource(path).toURI
-    val temperatures = Utils.readAverageTemperatures(file)
-    val prediction = Visualization.predictTemperature(temperatures, Location(0.0, 0.0))
+  test("predictTemperature local") {
+    //    val path = "/2000-local.csv"
+    //    val file = Main.getClass.getResource(path).toURI
+    //    val temperatures = Utils.readAverageTemperatures(file)
+    val temperatures = Iterable(
+      (berlin2, 20.0),
+      (berlin3, 10.0)
+    )
+    val prediction = Visualization.predictTemperature(temperatures, berlin4)
 
-    assert(prediction == 0)
+    assert(prediction == 15.0)
+  }
+
+  test("predictTemperature Germany") {
+    val temperatures = Iterable(
+      (berlin, 20.0),
+      (dresden, 5.0),
+      (munich, 5.0)
+    )
+    val prediction = Visualization.predictTemperature(temperatures, potsdam)
+
+    assert(prediction - 19.0 < 1)
   }
 }
