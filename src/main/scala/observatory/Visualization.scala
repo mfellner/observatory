271fc944ce04c73f91be658e0445b336242f5309
Maximilian Fellner <max.fellner@gmail.com>
2017-04-15 19:23:50 +0200

    feat: implement distance

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,11 +1,14 @@
 package observatory
 
-import com.sksamuel.scrimage.{Image, Pixel}
+import java.lang.Math._
+
+import com.sksamuel.scrimage.Image
 
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization {
+  val earthRadiusMeters = 6371000F
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
@@ -13,7 +16,32 @@ object Visualization {
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
+    val p = 2
+
+    temperatures.par.toStream.map {
+      case (Location(lat, lon), temp) => temp
+    }
+
     0
+  }
+
+  private def weight(interpolated: Location, sampled: Location) = {
+
+  }
+
+  // https://en.wikipedia.org/wiki/Great-circle_distance
+  def distance(a: Location, b: Location): Double = {
+    val dLat = toRadians(abs(a.lat - b.lat))
+    val dLon = toRadians(abs(a.lon - b.lon))
+    val aLatR = toRadians(a.lat)
+    val bLatR = toRadians(b.lat)
+
+    val dividend = sqrt(pow(cos(bLatR) * sin(dLon), 2) +
+      pow(cos(aLatR) * sin(bLatR) - sin(aLatR) * cos(bLatR) * cos(dLon), 2))
+    val divisor = sin(aLatR) * sin(bLatR) + cos(aLatR) * cos(bLatR) * cos(dLon)
+    //    val radians = atan(dividend / divisor)
+    val radians = atan2(dividend, divisor)
+    6371000 * radians
   }
 
   /**
--- src/test/scala/observatory/ManipulationTest.scala
+++ src/test/scala/observatory/ManipulationTest.scala
@@ -4,9 +4,19 @@ import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.prop.Checkers
+import org.scalactic.TolerantNumerics
 
 @RunWith(classOf[JUnitRunner])
 class ManipulationTest extends FunSuite with Checkers {
+  test("distance") {
+
+    val london = Location(51.509865, -0.118092)
+    val berlin = Location(52.520008, 13.404954)
+    val d = Visualization.distance(london, berlin)
+    implicit val doubleEquality = TolerantNumerics.tolerantDoubleEquality(2F)
+    assert(d === 930851F)
+  }
+
   test("predictTemperature") {
     val path = "/2000-local.csv"
     val file = Main.getClass.getResource(path).toURI
