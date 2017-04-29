4f8e7ba31e9a22dcf160ac547f192f6488313b71
Maximilian Fellner <max.fellner@gmail.com>
2017-04-29 17:15:13 +0200

    feat: implement makeGrid.

--- src/main/scala/observatory/Manipulation.scala
+++ src/main/scala/observatory/Manipulation.scala
@@ -1,9 +1,56 @@
 package observatory
 
+import java.lang.Math.pow
+
+import observatory.Visualization.{distanceSimple}
+
+import scala.collection.{GenIterable, GenMap}
+
 /**
   * 4th milestone: value-added information
   */
 object Manipulation {
+
+  class TemperatureGrid(temperatures: Iterable[(Location, Double)]) {
+
+    private val distanceThresholdMeters = 1000.0
+    private val p = 2.5
+
+    private lazy val weightMap: GenMap[Location, GenIterable[Double]] = {
+
+      def getWeights(location: Location) = temperatures.par.map {
+        case (loc, _) =>
+          val d = distanceSimple(loc, location)
+          if (d > distanceThresholdMeters)
+            1.0 / pow(d, p)
+          else
+            1.0
+      }
+
+      val locations = for {
+        lat <- -89 to 90
+        lon <- -180 to 179
+      } yield Location(lat, lon)
+
+      locations.par.map(loc => (loc, getWeights(loc))).toMap
+    }
+
+    def getAverageTemperature(lat: Int, lon: Int): Double = {
+      val location = Location(lat, lon)
+      val weights = weightMap(location)
+
+      val sumOfWeightedTemps = temperatures.par.zip(weights).map {
+        case ((loc, temp), weight) =>
+          val d = distanceSimple(loc, location)
+          if (d > distanceThresholdMeters) {
+            weight * temp
+          } else
+            temp
+      }.sum
+
+      sumOfWeightedTemps / weights.sum
+    }
+  }
 
   /**
     * @param temperatures Known temperatures
@@ -11,8 +58,11 @@ object Manipulation {
     *         returns the predicted temperature at this location
     */
   def makeGrid(temperatures: Iterable[(Location, Double)]): (Int, Int) => Double = {
-    ???
+    val grid = new TemperatureGrid(temperatures)
+    grid.getAverageTemperature
   }
+
+  //    (lat: Int, lon: Int) => Visualization.predictTemperature(temperatures, Location(lat, lon))
 
   /**
     * @param temperaturess Sequence of known temperatures over the years (each element of the collection
@@ -21,6 +71,9 @@ object Manipulation {
     */
   def average(temperaturess: Iterable[Iterable[(Location, Double)]]): (Int, Int) => Double = {
     ???
+    //    val n = temperaturess.size
+    //    (lat: Int, lon: Int) =>
+    //      temperaturess.toStream.par.map(makeGrid).map(gridFn => gridFn(lat, lon)).sum / n.toDouble
   }
 
   /**
@@ -28,7 +81,8 @@ object Manipulation {
     * @param normals      A grid containing the “normal” temperatures
     * @return A sequence of grids containing the deviations compared to the normal temperatures
     */
-  def deviation(temperatures: Iterable[(Location, Double)], normals: (Int, Int) => Double): (Int, Int) => Double = {
+  def deviation(temperatures: Iterable[(Location, Double)],
+                normals: (Int, Int) => Double): (Int, Int) => Double = {
     ???
   }
 
--- src/test/scala/observatory/ManipulationTest.scala
+++ src/test/scala/observatory/ManipulationTest.scala
@@ -7,4 +7,21 @@ import org.scalatest.prop.Checkers
 
 @RunWith(classOf[JUnitRunner])
 class ManipulationTest extends FunSuite with Checkers {
+  val testTemperatures = Array(
+    (Location(45, -90), 0.0),
+    (Location(45, 90), 32.0),
+    (Location(-45, 90), 32.0),
+    (Location(-45, -90), 0.0),
+    (Location(83.62348, -34.145508), 16.0),
+    (Location(83.7, -34.145508), -18.0)
+  )
+
+  test("makeGrid") {
+    val getTemp = Manipulation.makeGrid(testTemperatures)
+
+    assert(Math.abs(getTemp(45, -90)) < 0.001)
+    assert(getTemp(45, 90) === 32.0)
+    assert(getTemp(-45, 90) === 32.0)
+    assert(Math.abs(getTemp(-45, -90)) < 0.001)
+  }
 }
